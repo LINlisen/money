a31fd764283ef0ec19573faef1b9a63628278bac
LIN Linsen <z1x2c3v441@gmail.com>
2020-04-24 16:11:46 +0800

    first-0424

---
+++ src/compenents/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {StyleSheet,Text,View,Image,TouchableOpacity} from "react-native";
+const Home =({})=>{
+    return(
+
+    )
+};
+const styles = StyleSheet.create({
+   
+  });
+  
+  export default Home;
